81c895443858ccd082c8db2be60749dc070d3447
do nhat quang <donhatquang@gmail.com>
2014-10-01 19:14:28 +0800

    slide
    slide

--- modules/mod_slideshowck/mod_slideshowck.php
+++ modules/mod_slideshowck/mod_slideshowck.php
@@ -141,7 +141,7 @@ $captioncss = modSlideshowckHelper::createCss($params, 'captionstyles');
 $fontfamily = ($params->get('captionstylesusefont','0') && $params->get('captionstylestextgfont', '0')) ? "font-family:'" . $params->get('captionstylestextgfont', 'Droid Sans') . "';" : '';
 if ($fontfamily) {
 	$gfonturl = str_replace(" ", "+", $params->get('captionstylestextgfont', 'Droid Sans'));
-	$document->addStylesheet('https://fonts.googleapis.com/css?family=' . $gfonturl);
+	//$document->addStylesheet('https://fonts.googleapis.com/css?family=' . $gfonturl);
 }
 
 $css .= "
